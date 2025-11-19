cdf11c966192f510b29e04f6204c09f387170a12
Mateus Fernando <mateusfss368@gmail.com>
2025-11-19 19:35:24 -0300

    pagina pronta

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const startups = [
       'XYZ Solutions Plataforma que utiliza IoT e IA para monitorar consumo energético e reduzir custos em pequenas e médias empresas.',
     tecnologias: ['IoT', 'IA'],
     segmento: 'tecnologia',
-    imagem: 'img/xyz.png',
+    imagem: '/imagens/xyz.png',
     link: 'startup.html',
   },
   {
@@ -15,7 +15,7 @@ const startups = [
       'Saúde Digital Aplicativo de telemedicina que conecta pacientes a médicos especialistas, com suporte a prontuário eletrônico.',
     tecnologias: ['Cloud', 'Machine Learning'],
     segmento: 'saúde',
-    imagem: 'img/abconnect.png',
+    imagem: '/imagens/abconnect.png',
     link: '#',
   },
 ];
@@ -29,15 +29,35 @@ function render(lista) {
   area.innerHTML = '';
   lista.forEach((s) => {
     area.innerHTML += `
-			<div class="card">
-				<img src="${s.imagem}" width="80">
-				<h3>${s.nome}</h3>
-				<p>${s.descricao}</p>
-				${s.tecnologias.map((t) => `<span class="tag">${t}</span>`).join('')}
-				<br><br>
-				<a href="${s.link}">Saber mais</a>
-			</div>
-		`;
+      <div style="width:634px;min-height:316px;background:#fff;border-radius:20px;border:1px solid #BFBFBF;margin-bottom:32px;padding:28px;box-sizing:border-box;display:flex;flex-direction:column;">
+        <div style="display:flex;align-items:center;gap:28px;">
+          <img style="width:200px;height:200px;border-radius:16px;object-fit:cover;" src="${
+            s.imagem
+          }" />
+          <div style="width:348px;display:flex;flex-direction:column;gap:8px;">
+            <div style="color:#465EFF;font-size:24px;font-weight:600;">${
+              s.nome
+            }</div>
+            <div style="color:#535353;font-size:18px;">${
+              s.segmento.charAt(0).toUpperCase() + s.segmento.slice(1)
+            }</div>
+            <div style="color:#535353;font-size:18px;">${s.descricao}</div>
+            <div style="display:flex;gap:13px;">
+              ${s.tecnologias
+                .map(
+                  (t) => `
+                <div style="min-width:68px;height:35px;padding:3px 19px;background:#FEFEFE;border-radius:10px;outline:1px solid #535353;display:flex;align-items:center;justify-content:center;color:#535353;font-size:16px;font-weight:500;">${t}</div>
+              `
+                )
+                .join('')}
+            </div>
+          </div>
+        </div>
+        <div style="width:100%;height:27px;text-align:right;display:flex;align-items:center;justify-content:flex-end;color:#535353;font-size:14px;font-weight:500;text-decoration:underline;cursor:pointer;"> <a href="${
+          s.link
+        }" style="color:inherit;text-decoration:inherit;">Saber mais</a></div>
+      </div>
+    `;
   });
 }
 
